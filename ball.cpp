a760e7def8ece160985bd522a58120e528e531b1
Yousef Moazzam <mrbenzedrine@live.co.uk>
2018-03-23 17:35:08 +0000

    Add 'move' function to Ball class

--- ball.cpp
+++ ball.cpp
@@ -10,6 +10,34 @@ Ball::Ball(int initPosX, int initPosY, int initVelX, int initVelY, int screen_wi
 
 }
 
+void Ball::move(int screen_width, int screen_height)
+{
+
+    posX += velX;
+
+    if(posX < 0)
+    {
+        // player2 scores a point
+    }
+    else if(posX > screen_width - BALL_WIDTH)
+    {
+        // player1 scores a point
+    }
+
+    posY += velY;
+
+    if(posY < 0 || posY > screen_height - BALL_HEIGHT)
+    {
+        // Move the ball back, and reverse the sign of the
+        // y velocity component to make the ball 'bounce'
+        // off the bottom/top of the window
+
+        posY -= velY;
+        velY *= -1;
+    }
+
+}
+
 void Ball::render(SDL_Renderer* renderer)
 {
 
