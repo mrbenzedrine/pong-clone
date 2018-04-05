113cc700ca0d0ab64c0c7035555afaef88957093
Yousef Moazzam <mrbenzedrine@live.co.uk>
2018-04-05 19:00:21 +0100

    Add a default constructor and destructor to the Score class

--- score.cpp
+++ score.cpp
@@ -1,1 +1,28 @@
 #include "score.h"
+
+Score::Score()
+{
+
+    scoreValue = 0;
+    textTexture = NULL;
+
+}
+
+Score::~Score()
+{
+
+    free();
+
+}
+
+void Score::free()
+{
+
+    if(textTexture != NULL)
+    {
+        SDL_DestroyTexture(textTexture);
+        textTexture = NULL;
+        scoreValue = 0;
+    }
+
+}
--- score.h
+++ score.h
@@ -1,10 +1,17 @@
+#include <SDL2/SDL.h>
+
 class Score
 {
     public:
+        Score();
+        ~Score();
+
+        void free();
         void incrementScore(){scoreValue++;};
         int getScore() const {return scoreValue;};
 
     private:
         int scoreValue;
+        SDL_Texture* textTexture;
 
 };
